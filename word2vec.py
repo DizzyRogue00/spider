779bfd90fc5824d5adaa437ffa7820b393fef6a0
DizzyRogue00 <ZHOU0329@e.ntu.edu.sg>
2023-04-13 18:20:58 +0800

    word2vec updated construct vocabulary dictionary

--- word2vec.py
+++ word2vec.py
@@ -10,7 +10,14 @@ from datetime import timedelta
 MAX_VOCAB_SIZE=10000 # length of dictionary
 UNK,PAD="<UNK>","<PAD>" # unkown, padding symbol
 
-def build_vocab(file_path, tokenizer,max_size,min_freq):
+def build_vocab(file_path, tokenizer,max_size,min_freq): # construct the vocabuary dictionary
+    """
+    :param file_path:
+    :param tokenizer:
+    :param max_size:
+    :param min_freq:
+    :return:
+    """
     vocab_dic={}
     with open(file_path,'r',encoding="utf-8") as f:
         for line in tqdm(f):
@@ -30,4 +37,43 @@ def build_dataset(config,use_word):
         tokenizer=lambda x:x.split(' ') # word-level
     else:
         tokenizer=lambda x:[y for y in x] # char-level
-    if os.path
+    if os.path.exists(config.vocab_path):
+        vocab_dict=pkl.load(open(config.vocab_path))
+    else:
+        vocab_dict=build_vocab(config.train_path,tokenizer=tokenizer,max_size=MAX_VOCAB_SIZE,min_freq=1)
+        pkl.dump(vocab_dict,open(config.vocab_path,'wb'))
+    print(f"Vocabuary size: {len(vocab_dict)}")
+
+    def load_dataset(path,pad_size=32):
+        contents=[]
+        with open(path,'r',encoding="UTF-8") as f:
+            for line in tqdm(f):
+                lin=line.strip()
+                if not lin:
+                    continue
+                if len(lin.split('\t'))==2:
+                    content,label=lin.split('\t')
+                else:
+                    content=lin.split('\t')
+                token=tokenizer(content)
+                seq_len=len(token)
+                if seq_len<pad_size:
+                    token.extend([PAD]*(pad_size-seq_len))
+                else:
+                    token=token[:pad_size]
+                    seq_len=pad_size
+                #word to id
+                words=[vocab_dict.get(word,vocab_dict.get(UNK)) for word in token]
+                try:
+                    label
+                except NameError:
+                    label_exists=False
+                else:
+                    label_exists=True
+                if label_exists:
+                    contents.append((words,int(label),seq_len))
+                else:
+                    contents.append((words,seq_len))
+        return contents #[([...],0,32),([...],1,32)]
+
+    train=load_dataset()

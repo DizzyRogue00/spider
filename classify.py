63c91152b0fbe8e140042a6d3ad7352bdff1361c
周畅 <zhouchang@zhouchangs-MacBook-Pro-2.local>
2023-04-19 03:35:38 +0800

    update textCNN

--- classify.py
+++ classify.py
@@ -47,6 +47,35 @@ class config(object):
         self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
         self.num_filters = 256                                          # 卷积核数量(channels数)
 
+class TextCNN_trail(nn.Module):
+    def __init__(self,config):
+        super(TextCNN,self).__init__()
+        if config.embedding_pretrained is not None:
+            self.embedding=nn.Embedding.from_pretrained(config.embedding_pretrained,freeze=False)
+        else:
+            self.embedding=nn.Embedding(config.n_vocab,config.embed,padding_idx=config.n_vocab-1)
+        self.convs=nn.ModuleList(
+            [nn.Conv2d(1,config.num_filters,(k,config.embed)) for k in config.filter_sizes]
+        )
+        self.dropout=nn.Dropout(config.dropout)
+        self.classify=nn.Linear(config.num_filters*len(config.filter_sizes),config.num_classes)
+
+    def conv_activate_and_pool(self,x,conv):
+        x=F.relu(conv(x)).squeeze(3)
+        x=F.max_pool1d(x,x.size(2)).squeeze(2)
+        return x
+
+    def forward(self,x):#include batch_size
+        out=self.embedding(x[0])
+        #out=out.unsqueeze(0)#if no batch_size
+        out=out.unsqueeze(1)
+        out=torch.cat([self.conv_activate_and_pool(out,conv) for conv in self.convs],dim=1)
+        out=self.dropout(out)
+        out=self.classify(out)
+        return out
+
+
+
 class GlobalMaxPool1d(nn.Module):
     def __init__(self):
         super(GlobalMaxPool1d,self).__init__()
